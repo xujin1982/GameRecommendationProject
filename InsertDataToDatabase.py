5e320ed98ba319431508f2f2ac531e35e0ad3ad3
xujin1982 <junxiaozhu@gmail.com>
2017-01-05 18:04:57 -0600

    Update SQL script

--- InsertDataToDatabase.py
+++ InsertDataToDatabase.py
@@ -2,10 +2,76 @@ import pandas as pd
 import numpy as np
 import mysql.connector
 from sqlalchemy import create_engine
-import requests, json, os, time, sys, random
+from datetime import datetime
+import requests, json, os, time, sys, random, re
+
+path_steam_app_info = 'E:/GitHub/output/steam_app_info.csv'
+
+app_detail_file = '../output/2016-12-27_app_detail_test.txt'
+with open(app_detail_file, 'rb') as f:
+    dic_app_detail = {'name':{}, 'type':{}, 'initial_price':{}, 'release_date':{}, 'score':{}, 
+                      'recommendations':{}, 'windows':{}, 'mac':{}, 'linux':{}, 'header_image':{}}
+    lst_raw_string = f.readlines()
+    total_count = len(lst_raw_string)
+    count = 0
+    for line in lst_raw_string:
+        temp = json.loads(line)
+        tempid = temp.keys()[0]        
+        if temp[tempid]['success']:            
+            temp = temp[tempid].get('data')
+            steam_appid = int(temp.get('steam_appid'))
+            name = temp.get('name')
+            apptype = temp.get('type')
+
+            if temp.get('is_free'):
+                initial_price = 0
+            else:
+                initial_price = temp.get('price_overview',{}).get('initial')
+            if temp.get('release_date').get('coming_soon') == False:
+                temp_date = temp.get('release_date').get('date')
+                if not temp_date == '':
+                    if re.search(',',temp_date) == None:
+                        release_date = datetime.strptime(temp_date, '%b %Y')
+                    else:
+                        try:
+                            release_date = datetime.strptime(temp_date, '%b %d, %Y')
+                        except:
+                            pass
+                        try:
+                            release_date = datetime.strptime(temp_date, '%d %b, %Y')
+                        except:
+                            pass
+            score = temp.get('metacritic',{}).get('score')
+            recommendations = temp.get('recommendations',{}).get('total')
+            header_image = temp.get('header_image')
+            required_age = int(temp.get('required_age'))
+            if not temp.get('developers') == None:
+                developers = temp.get('developers')[0]
+            windows = 1 if temp.get('platforms').get('windows') else 0
+            mac = 1 if temp.get('platforms').get('mac') else 0
+            linux = 1 if temp.get('platforms').get('linux') else 0
+            count += 1            
+            dic_app_detail['name'].update({steam_appid:name})
+            dic_app_detail['type'].update({steam_appid:apptype})
+            dic_app_detail['initial_price'].update({steam_appid:initial_price})
+            dic_app_detail['release_date'].update({steam_appid:release_date})
+            dic_app_detail['score'].update({steam_appid:score})
+            dic_app_detail['recommendations'].update({steam_appid:recommendations})
+            dic_app_detail['windows'].update({steam_appid:windows})
+            dic_app_detail['mac'].update({steam_appid:mac})
+            dic_app_detail['linux'].update({steam_appid:linux})
+            dic_app_detail['header_image'].update({steam_appid:header_image})
+
+            
+df_app_detail = pd.DataFrame(dic_app_detail)
+df_app_detail.initial_price = df_app_detail.initial_price.map(lambda x: x/100.0)
+df_app_detail.index.name = 'steam_appid'
+df_app_detail = df_app_detail[['name', 'type', 'initial_price', 'release_date', 'score', 
+                               'recommendations', 'windows', 'mac', 'linux', 'header_image']]
+df_app_detail.reset_index(inplace=True)
+df_app_detail.to_csv(path_steam_app_info,encoding='utf8',index=False)
 
 engine = create_engine('mysql+mysqlconnector://root:1234@127.0.0.1/game_recommendation?charset=utf8mb4')
-
 engine.execute('''
     CREATE TABLE IF NOT EXISTS tbl_steam_app
     (
@@ -19,49 +85,28 @@ engine.execute('''
         windows BOOLEAN,
         mac BOOLEAN,
         linux BOOLEAN,
-        header_image VARCHAR(100),
-        required_age INT,
-        developers VARCHAR(500) CHARACTER SET utf8mb4
+        header_image VARCHAR(100)
     );
     ''')
 
-columns = ['steam_appid', 'name', 'type', 'initial_price', 'release_date', 'score', 
-        'recommendations', 'windows', 'mac', 'linux', 'header_image','required_age','developers']
-app_detail_data = pd.DataFrame(pd.DataFrame(np.array([np.nan] * len(columns))).T, columns = columns)
-
-app_detail_file = '../output/2016-12-27_app_detail_test.txt'
-with open(app_detail_file, 'rb') as f:
-    for line in f.readlines():
-        temp = json.loads(line)
-        tempid = temp.keys()[0]
-        if temp[tempid]['success']:
-            temp = temp[tempid]['data']
-            app_detail_data.steam_appid = int(temp['steam_appid'])
-            app_detail_data.name = temp['name']
-            app_detail_data.apptype = temp['type']
-            if temp['is_free']:
-                app_detail_data.initial_price = 0
-            else:
-                try:
-                    app_detail_data.initial_price = temp['price_overview']['initial']
-                except:
-                    pass
-            app_detail_data.release_date = temp['release_date']['date']
-            try:
-                app_detail_data.score = int(temp['metacritic']['score'])
-            except:
-                pass
-            try:
-                app_detail_data.recommendations = int(temp['recommendations']['total'])
-            except:
-                pass
-            app_detail_data.windows = temp['platforms']['windows']
-            app_detail_data.mac = temp['platforms']['mac']
-            app_detail_data.linux = temp['platforms']['linux']
-            app_detail_data.header_image = temp['header_image']
-            app_detail_data.required_age = int(temp['required_age'])
-            try:
-                app_detail_data.developers = temp['developers'][0]
-            except:
-                pass
-            app_detail_data.to_sql('tbl_steam_app',engine,if_exists='append',index = False)
+engine.execute('''
+    LOAD DATA INFILE '%s' INTO TABLE `tbl_steam_app` 
+    FIELDS TERMINATED BY ','
+    OPTIONALLY ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES
+    (@steam_appid, @name, @type, @initial_price, @release_date, @score, @recommendations, 
+    @windows, @mac, @linux, @header_image)
+    SET
+    steam_appid = nullif(@steam_appid, ''),
+    name = nullif(@name, ''),
+    type = nullif(@type, ''),
+    initial_price = nullif(@initial_price,''),
+    release_date = nullif(@release_date,''),
+    score = nullif(@score,''), 
+    recommendations = nullif(@recommendations, ''),
+    windows = nullif(@windows, ''),
+    mac = nullif(@mac, ''),
+    linux = nullif(@linux, ''),
+    header_image = nullif(@header_image, '');
+    ''' % path_steam_app_info)
